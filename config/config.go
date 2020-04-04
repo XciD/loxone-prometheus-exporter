29309f2aef3daec9eb2cf9dfd37abbaa0affe00d
Axel Gluth <mail@axelgluth.de>
2020-04-04 17:01:43 +0200

    Use viper to read config
    Enables config files and reading settings from environment variables

---
+++ config/config.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const envPrefix string = "LOXPROM"
+
+// ReadConfigErr is returned if something goes wrong while reading the config
+// We use this error to return a meaningful string
+// instaead of a viper error object
+type ReadConfigErr struct {
+	err string
+}
+
+func (e *ReadConfigErr) Error() string {
+	return e.err
+}
+
+// Config holds our config values
+type Config struct {
+	Host     string
+	User     string
+	Password string
+}
+
+// NewConfig reads the config into a new Config object
+func NewConfig() (*Config, error) {
+
+	cfg := new(Config)
+
+	// Flags
+	pflag.String("configFile", "", "Path and name of Config")
+	pflag.String("host", "", "URL of the Miniserver")
+	pflag.String("user", "", "Username for Miniserver")
+	pflag.String("password", "", "Password for Miniserver")
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+
+	// Config file
+	if file := viper.GetString("configFile"); file != "" {
+		viper.SetConfigFile(file)
+	} else {
+		viper.SetConfigName("loxone-prometheus-exporter")
+		viper.SetConfigType("yml")
+		viper.AddConfigPath(defaultConfigPath)
+	}
+	err := viper.ReadInConfig()
+	if err != nil {
+		switch err.(type) {
+		case viper.ConfigFileNotFoundError:
+			// It's not mandatory to have a config file
+		default:
+			return nil, &ReadConfigErr{fmt.Sprintf("Unable to read config file %s: %v", viper.GetViper().ConfigFileUsed(), err)}
+		}
+	}
+
+	// Environment Variables
+	viper.SetEnvPrefix(envPrefix)
+	viper.BindEnv("Host")
+	viper.BindEnv("User")
+	viper.BindEnv("Password")
+
+	err = viper.Unmarshal(cfg)
+	if err != nil {
+		return nil, &ReadConfigErr{fmt.Sprintf("Unable to marshal config: %v", err)}
+	}
+	return cfg, nil
+}
---
+++ config/config_other.go
@@ -0,0 +1,5 @@
+// +build !linux
+
+package config
+
+const defaultConfigPath string = "."
